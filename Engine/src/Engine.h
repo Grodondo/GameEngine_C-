380ea6038ce5371f155a02c974493b21641ebad3
Carlos Arroyo <dragon.carlos@outlook.es>
2024-08-25 10:44:54 +0200

    Setup basic application and entry point.

---
+++ Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Intended to be used for Engine Applications
+
+#include "Engine/Application.h"
+
+//---------------Entry-Point---------------
+#include "Engine/EntryPoint.h"
+//-----------------------------------------
