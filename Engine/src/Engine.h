4b7f56dd173decc5e9d9abc21800e8d02db9b49c
Grodondo <dragon.carlos@outlook.es>
2024-09-02 11:34:14 +0200

    Logger done

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -3,6 +3,7 @@
 // Intended to be used for Engine Applications
 
 #include "Engine/Application.h"
+#include "Engine/Log.h"
 
 //---------------Entry-Point---------------
 #include "Engine/EntryPoint.h"
--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -6,6 +6,10 @@ extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Engine::Log::Init();
+	ENGINE_CORE_WARN("Initialized Log");
+	ENGINE_INFO("Hello world");
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
